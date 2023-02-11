fb2df6f23dc926c19ba64e1db919ff119962cc2c
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-02-11 21:11:34 +1100

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+const input = document.querySelector("#new-task-description");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const task = input.value;
+  const taskNode = document.createElement("li");
+  taskNode.textContent = task;
+  document.querySelector("ul").appendChild(taskNode);
+});
 });
